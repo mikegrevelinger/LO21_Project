96f8925733701ac68d537c0df0e70377699755a7
bnogaret <bnogaret@users.noreply.github.com>
2014-05-20 08:55:53 +0200

    Update ErrorDB.cpp

--- root/src/class/ErrorDB.cpp
+++ root/src/class/ErrorDB.cpp
@@ -1,11 +1,15 @@
 #include "ErrorDB.h"
 
 ErrorDB::ErrorDB(const QString & message):info(message){
+    /*
+    --> cree dans gui une class messagePopUp qui le fait
     msgBox = new QMessageBox ;
     msgBox->setText(info);
     msgBox->setInformativeText("Veuillez rééssayer.");
     //msgBox->setStandardButtons(QMessageBox::Cancel);
     msgBox->exec();
+    */
+    
 }
 
 
